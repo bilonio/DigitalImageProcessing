e97c7d971a1d8156df9d6fdbfeb7a2025ec249bd
Vasilis Aitsidis <bilonio02@gmail.com>
2024-04-20 00:56:38 +0300

    add comments to global_hist_eq.py

--- histogramEqualization/global_hist_eq.py
+++ histogramEqualization/global_hist_eq.py
@@ -5,26 +5,30 @@ from PIL import Image
 
 def get_equalization_transform_of_img(img):
 
-    hist = custom_histogram(img)
-    u = [0] * len(hist)
-    y = [0] * len(hist)
-    u[0] = hist[0]
+    hist = custom_histogram(img)  # get the histogram of the image
+    u = [0] * len(hist)  # get the cumulative distribution function
+    y = [0] * len(hist)  # get the equalization transform
+    u[0] = hist[0]  # initialize the first element of the cdf
     y[0] = 0
-    for k in range(1, 256):
+    for k in range(1, 256):  # calculate the cdf and equalization transform
         u[k] = hist[k] + u[k - 1]
         y[k] = round(((u[k] - u[0]) / (1 - u[0])) * (256 - 1))
     equalization_transform = y
 
-    return equalization_transform
+    return equalization_transform  # return the equalization transform
 
 
 def perform_global_hist_transform(img):
-    equalized_img = np.zeros(img.shape)
-    equalized_img_hist = get_equalization_transform_of_img(img)
+    equalized_img = np.zeros(img.shape)  # initialize the equalized image
+    equalized_img_hist = get_equalization_transform_of_img(
+        img
+    )  # get the equalization transform of the image
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            equalized_img[i, j] = equalized_img_hist[img[i, j]]
-    return equalized_img
+            equalized_img[i, j] = equalized_img_hist[
+                img[i, j]
+            ]  # get the equalized image transformation of the pixel
+    return equalized_img  # return the equalized image
 
 
 def custom_histogram(img):
