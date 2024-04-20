d8f40a5fd62631cf29ddf926e6ca523d6230ae12
Vasilis Aitsidis <bilonio02@gmail.com>
2024-04-20 15:13:55 +0300

    create second figure for adaptive histogram

--- histogramEqualization/demo.py
+++ histogramEqualization/demo.py
@@ -22,13 +22,13 @@ img_array = np.array(bw_img)
 # get the histogram of the image
 hist, bins = np.histogram(img_array, bins=256, range=(0, 256), density=True)
 
-# get the histogram of equalised image
+# get the histogram of equalised image with global hist equalization
 equalized_hist = get_equalization_transform_of_img(img_array)
 
 # get the equalised image
 equalized_img = perform_global_hist_transform(img_array)
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(13, 7))
 
 plt.subplot(2, 2, 2)
 plt.imshow(img_array, cmap="gray")
@@ -56,19 +56,33 @@ plt.xlabel("Pixel Intensity")
 plt.ylabel("Frequency")
 
 plt.tight_layout()
-plt.show()
 
 # get the histogram of equalised image with contextual regions
-equalised_regions_histogram = calculate_eq_transformations_of_regions(
+equalised_regions_transforms = calculate_eq_transformations_of_regions(
     img_array, region_len_h=36, region_len_w=48
 )
+
 
 # get the equalised image with contextual regions
 equalized_img_regions = perform_adaptive_hist_equalization(
     img_array, region_len_h=36, region_len_w=48
 )
+regions_hist, region_bins = np.histogram(equalized_img_regions.ravel(), bins=256, range=(0, 256))
 # Create a new figure
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(13, 7))
+
+plt.subplot(1,2,2)
 plt.imshow(equalized_img_regions, cmap="gray")
-plt.title("Equalised Image with contextual regions")
+plt.title("Image with adaptive equalization")
+plt.axis("off")
+
+plt.subplot(1,2,1)
+plt.bar(region_bins[:-1], regions_hist, width=1)  
+plt.title('Histogram with adaptive equalization')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Probability Density')
+
+
+plt.tight_layout()
 plt.show()
+
